9fa85b0610f2b5d993d35433e0423f8de564337e
leezche <red2cracker@gmail.com>
2017-12-29 10:00:47 +0900

    func.js 파일로 분리.

---
+++ js/func.js
@@ -0,0 +1,7 @@
+		     $("#no-fouc").delay(250).animate({"opacity": "1"}, 250);
+		 
+             $(document).ready(function () {
+                 $('#sidebarCollapse').on('click', function () {
+                     $('#sidebar').toggleClass('active');
+                 });
+             });
